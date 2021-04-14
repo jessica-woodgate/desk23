13a2b26355d638779e66fe9c2d0f7dd9457f7089
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-04-14 13:42:32 +0100

    testing upload to database working

--- dbTempFile.js
+++ dbTempFile.js
@@ -28,51 +28,60 @@ var db = mongoose.connection;
 //bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+//array of instances of models
+//(have a separate model for every object/group of related information)
+var literacyData = []
+
+function literacyCreate(country, code, year, literacyRates, cb) {
+  //creating a new instance of the model
+  var data = new LiteracyModel({country:country, code:code, year:year, literacyRates:literacyRates});
+  //save the new model instance, passing a callback
+  data.save(function (err) {
+    if (err) {
+      cb(err, null)
+      return
+    }
+    //data saved
+    console.log('New Data: ' + data);
+    literacyData.push(data)
+    cb(null, data)
+  });
+}
+
 //populating database with data
 function createLiteracyRates(cb){
    async.parallel([
+      /*function(callback) {
+         literacyCreate('Afghanistan', 'AFG', 1979, 18.15768, callback);
+      },
+      function(callback){
+         literacyCreate('Albania', 'ALB', 2001, 98.71298, callback);
+      },
+      function(callback){
+         literacyCreate('Algeria', 'DZA', 1987, 49.63088, callback);
+      },
+      function(callback){
+         literacyCreate('American Samoa', 'ASM', 1980, 97.34416, callback);
+      },*/
       function(callback){
          LiteracyModel.insertMany(crossCountryLiteracyRates);
-      },
-      //checking that the data has been uploaded (remove when testing unnecessary)
-      function(callback){
-         LiteracyModel.find({'Entity':'World'}, 'Entity Code Data', function(err, results){
-            if(err) {
-               return handleError(err)
-            }else{
-               console.log(results);
-            }
-         })
       }
    ],
    //optional callback
    cb);
 }
 
-function findLiteracyRates(cb){
-   async.parallel([
-      function(callback){
-         LiteracyModel.find({'Entity':'World'}, 'Entity Code Data', function(err, results){
-            if(err) {
-               return handleError(err)
-            }else{
-               console.log(results);
-            }
-         })
-      }
-   ])
-}
-
 async.series([
-   createLiteracyRates,
-   //findLiteracyRates
+   createLiteracyRates
+   //LiteracyModel.insertMany(crossCountryLiteracyRates)
 ],
 //optional callback
 function(err, results) {
    if(err){
       console.log('FINAL ERR: '+err);
    }else{
-      console.log('LiteracyRates: '+results);
+      console.log('LiteracyRates: '+literacyData);
+      //console.log('data from file: '+dataFromFile);
    }
    // All done, disconnect from database
     mongoose.connection.close();

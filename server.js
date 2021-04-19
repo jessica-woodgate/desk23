3290eeb7ce37b95ca8a5f78eb8106146361c1530
Wenda Zhao <ed20979@bristol.ac.uk>
2021-04-19 03:16:59 +0100

    Added some server functions, more to be added.

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const path = require('path');
 const http = require('http');
 const bodyParser = require('body-parser');
 
+
+
 // Setup database
-const db = require('./db')
+//const db = require('./db') 
 
 // Get our API routes
 const api = require('./server/routes/api');
@@ -14,18 +16,70 @@ const app = express();
 
 // Parsers for POST data
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Point static path to dist (folder where build files are located)
 app.use(express.static(path.join(__dirname, 'dist/LiteracyRates')));
 
+/*****************************************
+//For database
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/dataPopulation');
+mongoose.Promise = global.Promise;
+********************************************/
+app.use(express.json());
+
 // Set our api routes
 app.use('/api', api);
+
+
 
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/LiteracyRates/index.html'));
 });
+
+//Error treatment
+app.use(function (err, req, res, next) {
+    res.status(422).send({ error: err.message });
+});
+
+app.get("/dataPopulation", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/Entity/:id", (request, response) => {
+    collection.findOne({ "_Entity": new ObjectId(request.params.id) }, (error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/LiteracyRates/:id", (request, response) => {
+    collection.findOne({ "_Literacy_rates": new ObjectId(request.params.id) }, (error, result) => {
+        if (error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get('/get_Country_name', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+        Country_name: req.query.country_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
 
 /**
  * Get port from environment and store in Express.
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const data = require('./data.json')
 
+/**********************
+//const Literacy = require('./dataPopulation')
+router.get('/Literacy', function (req, res, next) {
+    Literacy.find({}).then(function (dataPopulation) {
+        res.send(dataPopulation);
+    }).catch(next);
+});
+************************/
+
 /* GET api listing. */
 router.get('/', function (req, res) {
   res.header("Content-Type",'application/json');

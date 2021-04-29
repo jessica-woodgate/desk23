88c173665453ce0cc8efab60682bbf0b610bd647
Hamza Qureshi <qureshi.a.hamza@gmail.com>
2021-04-29 10:30:05 +0100

    adding hello to server.js

--- server.js
+++ server.js
@@ -57,3 +57,5 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
+
+console.log("hello");

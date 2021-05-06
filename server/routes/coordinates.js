e0898deda1ccc0da9267ee09c67c6111b9438ee0
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-05-06 11:14:44 +0100

    threejs website

--- server/routes/coordinates.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dbController = require('../../dbController.js');
-
-router.get('/getCoordinates', function(req, res){
-   dbController.listCoordinates(req, res);
-});
-
-module.exports = router;

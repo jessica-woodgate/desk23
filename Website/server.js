c878212e7a6530e6d7b31a4659cb6918e704ce1e
jess-mw <45073537+JMWoodgate@users.noreply.github.com>
2021-05-06 11:12:30 +0100

    removed hello

--- Website/server.js
+++ Website/server.js
@@ -61,5 +61,3 @@ const server = http.createServer(app);
  * Listen on provided port, on all network interfaces.
  */
 server.listen(port, () => console.log(`API running on localhost:${port}`));
-
-console.log("hello");
